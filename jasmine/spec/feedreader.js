5203fda92d0ce07604f96488f7519847ae5241fd
opalnovae <jnbostow@gmail.com>
2018-10-15 10:41:28 -0700

    Testing Jasmine Issues

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,34 +27,50 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        allFeeds.forEach(function(feed) {
+            /* TODO: Write a test that loops through each feed
+             * in the allFeeds object and ensures it has a URL defined
+             * and that the URL is not empty.
+             */
+            it('allFeeds object has URL defined and the URL is not empty', function() {
+                expect(feed.url).toBeDefined()
+                expect(feed.url.length).not.toBe(0)
+            });
+            /* TODO: Write a test that loops through each feed
+             * in the allFeeds object and ensures it has a name defined
+             * and that the name is not empty.
+             */
+            it('allFeeds object has name defined and the name is not empty', function() {
+                expect(feed.name).toBeDefined()
+                expect(feed.name.length).not.toBe(0)
+            })
+        })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        // const slideMenu = document.getElementsByClassName('slide-menu')[0]
+        // console.log(slideMenu.getBoundingClientRect().x < 0)
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+
+        it('testing boolean logic', function () {
+            expect(false).toBe(true)
+        })
+
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+
+        /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,10 +79,11 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    })
 }());
